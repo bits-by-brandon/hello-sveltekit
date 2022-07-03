c100ed78f1215d1b2aeb41eced97371509279cfb
Brandon Chang <brandon@brandonchang.me>
2022-07-03 11:44:06 -0700

    prod base pase

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -11,8 +9,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({ precompress: true }),
+		paths: {
+			base: '/hello-sveltekit'
+		},
 		prerender: { default: true },
-		trailingSlash: "always",
+		trailingSlash: 'always'
 	}
 };
 
