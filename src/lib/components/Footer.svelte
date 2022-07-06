4885d2ca21547c35d4d410c71687dc61e572055b
Brandon Chang <brandon@brandonchang.me>
2022-07-05 19:31:21 -0700

    lang store ref

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { t } from 'svelte-intl-precompile';
 	import { page } from '$app/stores';
-	import { language } from '$lib/stores/languageStore.js';
+	import { language } from '$lib/stores/languageStore.ts';
 	import Link from '$lib/components/Link.svelte';
 	import logo from './godot_logo.svg';
 </script>
